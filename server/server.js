3438460318fc81126d03ef5aa0fb2712c3058ecc
Luis Fernández <luis.brfernandez@gmail.com>
2017-07-04 00:53:37 -0500

    Added DEL /users/me/token

--- server/server.js
+++ server/server.js
@@ -125,6 +125,10 @@ app.post('/users',(req,res)=>{
   });
 });
 
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+});
+
 app.post('/users/login',(req,res)=>{
   var body = _.pick(req.body,['email','password']);
   console.log('Email', body.email);
@@ -145,8 +149,12 @@ app.post('/users/login',(req,res)=>{
 
 });
 
-app.get('/users/me',authenticate,(req,res)=>{
-  res.send(req.user);
+app.delete('/users/me/token', authenticate, (req,res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 app.listen(PORT,()=>{
