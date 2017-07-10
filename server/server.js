56ac7f1ec43352262e2905f831c0317135cb77d7
Luis Fernández <luis.brfernandez@gmail.com>
2017-07-10 12:25:56 -0500

    Modified way to declare headers

--- server/server.js
+++ server/server.js
@@ -17,6 +17,26 @@ var {authenticate} = require('./middleware/authenticate');
 var app = express();
 app.use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.post('/todos',authenticate, async (req,res) => {
 
   var todo = new Todo({
@@ -163,10 +183,9 @@ app.post('/users/login', async (req,res) => {
     const user = await User.findByCredentials(body.email,body.password);
     const token = await user.generateAuthToken();
     res.header('x-auth',token)
-    .header('Access-Control-Allow-Origin','*')
     .send(user);
   } catch (e) {
-    res.status(400).header('Access-Control-Allow-Origin','*').send();
+    res.status(400).send();
   }
 });
 
