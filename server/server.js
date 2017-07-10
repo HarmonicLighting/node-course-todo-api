441de2d78e17f958dcd60e91184955abbe65a29c
Luis Fernández <luis.brfernandez@gmail.com>
2017-07-10 12:52:16 -0500

    Added access-control-expose-headers header

--- server/server.js
+++ server/server.js
@@ -27,11 +27,14 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-auth');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Request headers you wish to allow to be exposed
+    res.setHeader('Access-Control-Expose-Headers', 'x-auth');
 
     // Pass to next layer of middleware
     next();
